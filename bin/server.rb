ce004022a52ca33ebf8700d04ad53355032b842e
jacjac3 <jjanchoi@uci.edu>
2009-08-19 15:13:58 -0700

    Cider Buffer still has issue with start, end pointers simulation created and place on server.rb for now

--- bin/server.rb
+++ bin/server.rb
@@ -1,7 +1,26 @@
+############### UNCOMMENT THIS TO START SERVER
+#
+#$LOAD_PATH.unshift "#{File.dirname(__FILE__)}/../lib"
+#require 'martinelli'
+
+#server = Martinelli::SerialDeviceWebServer.new
+#server.run.join
+################
+
+require 'martinelli/cider_buffer'
 
 
-$LOAD_PATH.unshift "#{File.dirname(__FILE__)}/../lib"
-require 'martinelli'
-
-server = Martinelli::SerialDeviceWebServer.new
-server.run.join
+puts "starting buffer"
+buffer = CiderBuffer.new(100)
+i = 0
+time = Time.new.to_f
+while i < 1000
+  buffer.insert(i)
+  i += 1
+  if(rand > 0.5)
+    temp = buffer.get(time)
+    ## buffer.get returns [data, lastTime]
+    puts temp[0]
+    time = temp[1]
+  end
+end
