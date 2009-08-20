d6e428db2209c98633352bc50e30b8510b94311f
jacjac3 <jjanchoi@uci.edu>
2009-08-20 14:20:03 -0700

    The test simulations works up to the last call. currently investigating the issue. run sever.rb to start simulation

--- bin/server.rb
+++ bin/server.rb
@@ -12,17 +12,26 @@ require 'martinelli/cider_buffer'
 
 
 puts "starting buffer"
-buffer = CiderBuffer.new(100)
+buffer = CiderBuffer.new(500)
 i = 0
 time = Time.new.to_f
 while i < 1000
   buffer.insert(i)
   i += 1
-  if(rand > 0.9)
+  if(rand(10) > 8)
     temp = buffer.get(time)
-    puts temp
-    puts "end of chunk \n"
     time = Time.new.to_f
+    if temp == nil
+      puts "time is syncronizing"
+    else
+      puts temp
+      puts "end of chunk \n"
+    end
   end
 end
-puts "end is = " + i
+puts "myTime: " + time.to_s + "\n"
+puts "startTime: " + buffer.getTimeStart.to_s + "\n"
+puts "endTime: " + buffer.getTimeEnd.to_s + "\n"
+puts "last data chunk \n"
+puts buffer.get(time)
+puts "end is = " + i.to_s
