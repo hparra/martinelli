91e5b4bb0fd69dd9cce36a2158126b172a506493
jacjac3 <jjanchoi@uci.edu>
2009-08-21 17:39:44 -0700

    Found issue when chunk size is 1. Trying to resolve issue.

--- bin/server.rb
+++ bin/server.rb
@@ -14,24 +14,40 @@ require 'martinelli/cider_buffer'
 puts "starting buffer"
 buffer = CiderBuffer.new(500)
 i = 0
-time = Time.new.to_f
+bool = true
 while i < 1000
   buffer.insert(i)
   i += 1
-  if(rand(10) > 8)
-    temp = buffer.get(time)
-    time = Time.new.to_f
+  if(bool)
+    bool = false
+    temp = buffer.getNew()
     if temp == nil
       puts "time is syncronizing"
     else
+      time = temp.pop
+      puts temp
+      puts "end of chunk \n"
+    end
+  end
+  if(rand(10) > 8)
+    temp = buffer.get(time)
+    if temp == nil
+      puts "time is syncronizing"
+    else
+      time = temp.pop
       puts temp
       puts "end of chunk \n"
     end
   end
 end
+temp = buffer.get(time)
+if (temp != nil)
+  puts "not nil"
+  temp.pop
+end
 puts "myTime: " + time.to_s + "\n"
 puts "startTime: " + buffer.getTimeStart.to_s + "\n"
 puts "endTime: " + buffer.getTimeEnd.to_s + "\n"
 puts "last data chunk \n"
-puts buffer.get(time)
+puts temp
 puts "end is = " + i.to_s
